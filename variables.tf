8459571cfaddf6a8023afe07ab0a5006f03d5e7b
Sridhar <sridharlinux2015@gmail.com>
2020-11-16 13:29:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,57 @@
-variable "tablename" {}
-variable "hashkey_name" {}
-variable "rangekey_name" {}
-variable "hashkey_type" {}
-variable "rangekey_type" {}
+variable "table_name" {
+  type        = string
+  default     = ""
+  description = "table name, e.g. 'app' or 'jenkins'"
+}
+
+variable "hash_key" {
+  type        = string
+  description = "DynamoDB table Hash Key"
+}
+
+variable "hash_key_type" {
+  type        = string
+  default     = "S"
+  description = "Hash Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
+}
+
+variable "range_key" {
+  type        = string
+  default     = ""
+  description = "DynamoDB table Range Key"
+}
+
+variable "range_key_type" {
+  type        = string
+  default     = "S"
+  description = "Range Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
+}
+
+/*
 variable "global_secondary_index_name" {}
+
+variable "global_secondary_index_map" {
+  type = list(object({
+    hash_key           = string
+    name               = string
+    non_key_attributes = list(string)
+    projection_type    = string
+    range_key          = string
+    read_capacity      = number
+    write_capacity     = number
+  }))
+  default     = []
+  description = "Additional global secondary indexes in the form of a list of mapped values"
+}
+
+variable "local_secondary_index_map" {
+  type = list(object({
+    name               = string
+    non_key_attributes = list(string)
+    projection_type    = string
+    range_key          = string
+  }))
+  default     = []
+  description = "Additional local secondary indexes in the form of a list of mapped values"
+}
+*/
