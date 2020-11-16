bdac57481e08386ccc1bfbbb2efce4d4ebc56901
Sridhar <sridharlinux2015@gmail.com>
2020-11-16 18:51:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,6 +34,46 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     }
   }
   
+  local_secondary_index_names {
+    count = (var.enabled ? 1 : 0) * length(var.local_secondary_index_map)
+  
+  # Convert the multi-item `local_secondary_index_map` into a simple `map` with just one item `name` since `triggers` does not support `lists` in `maps` (which are used in `non_key_attributes`)
+    triggers = {
+    "name" = var.local_secondary_index_map[count.index]["name"]
+    }
+  }
+/*
+  dynamic "attribute" {
+    for_each = local.attributes_final
+    content {
+      name = attribute.value.name
+      type = attribute.value.type
+    }
+  }
+  
+*/
+  dynamic "global_secondary_index" {
+    for_each = var.global_secondary_index_map
+    content {
+      hash_key           = global_secondary_index.value.hash_key
+      name               = global_secondary_index.value.name
+      non_key_attributes = lookup(global_secondary_index.value, "non_key_attributes", null)
+      projection_type    = global_secondary_index.value.projection_type
+      range_key          = lookup(global_secondary_index.value, "range_key", null)
+      read_capacity      = lookup(global_secondary_index.value, "read_capacity", null)
+      write_capacity     = lookup(global_secondary_index.value, "write_capacity", null)
+    }
+  }
+
+  dynamic "local_secondary_index" {
+    for_each = var.local_secondary_index_map
+    content {
+      name               = local_secondary_index.value.name
+      non_key_attributes = lookup(local_secondary_index.value, "non_key_attributes", null)
+      projection_type    = local_secondary_index.value.projection_type
+      range_key          = local_secondary_index.value.range_key
+    }
+  }
   
 /*
   global_secondary_index {
